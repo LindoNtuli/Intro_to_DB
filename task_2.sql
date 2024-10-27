9d4f453dec0e4e1bc48b0949de9a1f888ce77b54
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-10-28 01:24:16 +0200

    task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -42,8 +42,8 @@ CREATE TABLE Order_details (
     order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL,
+    quantity DOUBLE NOT NULL,
     price DECIMAL(10, 2),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
